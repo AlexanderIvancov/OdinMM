f840ee638b5a5fe5bc25398b953fb473c2f4d282
Alexander <Alexander@HOME>
2024-02-28 10:35:21 +0200

    Undo _Small

--- Odin/Global_Resourses.Designer.cs
+++ Odin/Global_Resourses.Designer.cs
@@ -4423,6 +4423,16 @@ namespace Odin {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap undo_small {
+            get {
+                object obj = ResourceManager.GetObject("undo_small", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Undo1 {
             get {
                 object obj = ResourceManager.GetObject("Undo1", resourceCulture);
