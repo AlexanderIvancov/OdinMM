80707124c1d7d2975d224170dc76c594f761f2a4
Alexander <Alexander@HOME>
2024-04-12 12:25:45 +0300

    Quot rashiril

--- Odin/Sales/frm_AddQuotation.Designer.cs
+++ Odin/Sales/frm_AddQuotation.Designer.cs
@@ -34,6 +34,8 @@
             // ctl_QuotDets1
             // 
             this.ctl_QuotDets1.ArtId = 0;
+            this.ctl_QuotDets1.Blocked = 0;
+            this.ctl_QuotDets1.CheckRMQP = 0;
             this.ctl_QuotDets1.Comments = "";
             this.ctl_QuotDets1.CreatAt = "";
             this.ctl_QuotDets1.CreatBy = "";
@@ -42,29 +44,37 @@
             this.ctl_QuotDets1.CustId = 0;
             this.ctl_QuotDets1.CustLine = "";
             this.ctl_QuotDets1.CustOrder = "";
+            this.ctl_QuotDets1.DelivAddressId = 0;
+            this.ctl_QuotDets1.DelivPlaceId = 0;
             this.ctl_QuotDets1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ctl_QuotDets1.EndCustomerId = 0;
             this.ctl_QuotDets1.ExpDate = "";
             this.ctl_QuotDets1.ExpDateD = new System.DateTime(((long)(0)));
+            this.ctl_QuotDets1.Internal = 0;
             this.ctl_QuotDets1.IsCopy = 0;
+            this.ctl_QuotDets1.IsProject = 0;
             this.ctl_QuotDets1.IsSent = 0;
             this.ctl_QuotDets1.Location = new System.Drawing.Point(0, 0);
             this.ctl_QuotDets1.Name = "ctl_QuotDets1";
             this.ctl_QuotDets1.NotPaidAdvance = 0;
             this.ctl_QuotDets1.PCB = 1;
+            this.ctl_QuotDets1.Primary = 0;
             this.ctl_QuotDets1.Qty = 0D;
             this.ctl_QuotDets1.Quotation = "";
             this.ctl_QuotDets1.QuotId = 0;
             this.ctl_QuotDets1.ReqDate = "05.09.2019";
             this.ctl_QuotDets1.ReqDateD = new System.DateTime(2019, 9, 5, 14, 23, 24, 595);
+            this.ctl_QuotDets1.Resale = 0;
             this.ctl_QuotDets1.Revision = "";
-            this.ctl_QuotDets1.SentDate = "05.09.2019 14:23:24";
+            this.ctl_QuotDets1.SentDate = "05.09.2019 14:23:24 ";
             this.ctl_QuotDets1.SentDateD = new System.DateTime(2019, 9, 5, 14, 23, 24, 573);
-            this.ctl_QuotDets1.Size = new System.Drawing.Size(548, 415);
+            this.ctl_QuotDets1.Size = new System.Drawing.Size(548, 469);
+            this.ctl_QuotDets1.Spoilage = 0D;
             this.ctl_QuotDets1.StateId = 0;
             this.ctl_QuotDets1.TabIndex = 0;
             this.ctl_QuotDets1.UnitId = 0;
             this.ctl_QuotDets1.UnitPrice = 0D;
+            this.ctl_QuotDets1.ValidBOM = 0;
             this.ctl_QuotDets1.Week = "W  .";
             this.ctl_QuotDets1.SendQuotId += new Odin.Sales.QuotIdSendingEventHandler(this.ctl_QuotDets1_SendQuotId);
             // 
@@ -73,13 +83,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(548, 415);
+            this.ClientSize = new System.Drawing.Size(548, 469);
             this.Controls.Add(this.ctl_QuotDets1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "frm_AddQuotation";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Add quotation";
-            this.ResumeLayout(false); this.GetKryptonFormFields(this.GetType());
+            this.ResumeLayout(false);
 
         }
 
